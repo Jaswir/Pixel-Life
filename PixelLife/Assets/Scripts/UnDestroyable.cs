bfecf41b1713762c6fc6e3564aeea74f82d704cf
Jaswir <jaswirraghoe@gmail.com>
2017-06-10 23:24:36 +0200

    Current, working on background music

---
+++ PixelLife/Assets/Scripts/UnDestroyable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnDestroyable : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        if (SceneManager.GetActiveScene().name == "RealizationNowhere")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
