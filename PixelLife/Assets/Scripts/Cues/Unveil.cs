e08982f94ee66907306d341d2a8f785b28012784
Jaswir <jaswirraghoe@gmail.com>
2017-07-03 22:29:00 +0200

    Bugs reduced?

--- PixelLife/Assets/Scripts/Cues/Unveil.cs
+++ PixelLife/Assets/Scripts/Cues/Unveil.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 
 public class Unveil : MonoBehaviour
 {
-    private bool playerMoved;
-    public GameObject hidden;
+    public SpriteRenderer noDoorSpriteRenderer;
+    public Sprite withDoor;
     public string soundeffect_key;
 
     // Update is called once per frame
     void Update()
     {
-
-        if (!playerMoved)
-        {
-            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.UpArrow) ||
+            if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.UpArrow) ||
                 Input.GetKey(KeyCode.W)
                 || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.RightArrow) ||
                 Input.GetKey(KeyCode.D))
             {
-                
-                hidden.SetActive(false);
-                SoundEffector.Instance.play(soundeffect_key);
 
-                playerMoved = true;
+                noDoorSpriteRenderer.sprite = withDoor;
+                SoundEffector.Instance.play(soundeffect_key);
                 Destroy(this);
             }
-        }
-
-
-
     }
 
 
